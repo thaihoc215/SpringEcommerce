f8f712bfb4b5afb4d110c3d50f706e781be4b4e6
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2018-04-10 01:24:51 +0700

    Create spring mvc config class

---
+++ SpringMVCAnnotationShoppingShoes/src/main/java/org/hochnt/springmvcshop/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.hochnt.springmvcshop.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
